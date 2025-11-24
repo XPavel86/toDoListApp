96337af81632194f4895d2ad06e85b3b5cbb0c0b
Pavel <rezusx@mail.ru>
2025-11-24 20:29:03 +0300

    Initial Commit

---
+++ toDoListApp/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  toDoListApp
+//
+//  Created by Pavel Dolgopolov on 24.11.2025.
+//
+
+import Foundation
---
+++ toDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListApp
+//
+//  Created by Pavel Dolgopolov on 24.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
