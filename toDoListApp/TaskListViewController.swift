75cb7c3a32f1c99cd4071e1e489a1ce3dac5b0c5
Pavel <rezusx@mail.ru>
2025-11-25 12:39:12 +0300

    giood view

--- toDoListApp/TaskListViewController.swift
+++ toDoListApp/TaskListViewController.swift
@@ -1,71 +1,29 @@
 //
-//  TaskListViewController_.swift
+//  TaskListViewController.swift
 //  toDoListApp
 //
 //  Created by Pavel Dolgopolov on 24.11.2025.
 //
-// TaskListViewController.swift
+
 import UIKit
 
-// ИЗМЕНЕНИЕ: Наследуемся от UITableViewController
-class TaskListViewController: UITableViewController {
-
-    // УБРАЛИ: @IBOutlet var tableView: UITableView! - он уже есть в UITableViewController
-    
-    // Экземпляр нашей ViewModel
-    private let viewModel = TaskListViewModel()
+class TaskListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Список дел"
-        
-        // УБРАЛИ: setupTableView() - больше не нужно
-        
-        setupBindings()
-        
-        // Загружаем данные
-        viewModel.fetchTasks()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        viewModel.fetchTasks()
-    }
-    
-    // УБРАЛИ: private func setupTableView() - больше не нужно
-    
-    private func setupBindings() {
-        viewModel.onDataUpdated = { [weak self] in
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-            }
-        }
-    }
-}
 
-// MARK: - UITableViewDataSource & UITableViewDelegate
+        // Do any additional setup after loading the view.
+    }
+    
 
-extension TaskListViewController {
-    
-    // UITableViewController уже реализует методы dataSource/delegate,
-    // нам нужно только переопределить нужные.
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfRowsInSection()
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Идентификатор должен совпадать со Storyboard
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCellIdentifier", for: indexPath) as! TaskTableViewCell
-        
-        let task = viewModel.task(at: indexPath)
-        cell.configure(with: task)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        print("Выбрана задача: \(viewModel.task(at: indexPath).title)")
-    }
+    */
+
 }
